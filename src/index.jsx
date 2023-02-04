ac0ced69d2ce376c37776f47a0b9196f17544fb6
smartwayswork <smartwayswork@gmail.com>
2023-02-04 11:56:08 +0000

    revert to wrking button

--- src/index.jsx
+++ src/index.jsx
@@ -17,12 +17,12 @@ const issues = [
 
 const timenow = Date.now();
 console.log('Starting ::: forge-react-issues ::::');
-      console.log('Timenow :' + timenow);
+      console.log('Starting :' + timenow);
 
 const App = () => {
   return (
     <Fragment>
-      <Text>Hey world 2 again - 1 feb  2023 </Text>  
+      <Text>Hey world 2 again - 11 jan 2023 </Text>  
       <Text>{timenow.Date}</Text>      
       <SectionMessage title="Playground" appearance="info">
         <Text>Some text from section</Text>
@@ -41,7 +41,7 @@ const App = () => {
           <Text>Hello yo</Text>
       </Tab>
       <Tab label="Tab 2">
-          <Text id= "tab2"> World again!</Text>
+          <Text>World again!</Text>
       </Tab>
     </Tabs>
 
@@ -55,40 +55,13 @@ export const run = render(
   </GlobalPage>
 )
 
-    // Define recursive function to print nested values
-    function printValues(obj) {
-      for(var k in obj) {
-          if(obj[k] instanceof Object) {
-              printValues(obj[k]);
-          } else {
-              console.log(obj[k] + "\n");
-          };
-      }
-    };
-
-
-// Define recursive function to print nested values - diff implementation 
-function printValues2(jsonString) { 
-  let res = [];
-  const inputObject = JSON.parse(jsonString);
-  Object.keys(inputObject).forEach(key => {
-    const value = inputObject[key];
-    if (typeof value === 'object') {
-      res.push(printValues2(JSON.stringify(value)));
-      
-    } else {
-      res.push(value);
-      console.log(JSON.stringify(value));
-    }
-  });
-  return res;
-}
-
 // API call to get JIRA issue
 async function callJIRA() {
-  console.log("CALLING JIRA - inside function: ");   
+  console.log("CALLING JIRA - inside function: "); 
+  //const response = await api.asUser().requestJira(`https://adihere.atlassian.net/browse/HEL-1?jql=project = HEL`);
   const response = await api.asUser().requestJira(route `/rest/api/3/search?jql=project = HEL`);
-  const issueDataJSON = await response.json(); //... extract from response
+  //const response = await api.asUser().requestJira(route `/rest/api/3/events`);
+  const issueData = await response.json(); //... extract from response
   
   if (response.status >= 400) {
       //console.error(response.status,response.statusText);
@@ -97,13 +70,13 @@ async function callJIRA() {
   
   else
   {     
-    //const callJIRAtext = await response.text;
-    console.log(`Successful Response: ${response.status} ${response.statusText}`);      
-    console.log("Spitting out JSON: \n" + issueDataJSON +"\n");
-    //console.log(issueDataJSON.issues.map(function(issue) {return issue.key}));
-    //objJSON = JSON.parse(issueDataJSON);
-    
-    printValues2(issueDataJSON);
+  //const callJIRAtext = await response.text;
+  console.log(`Successful Response: ${response.status} ${response.statusText}`);  
+  console.log("Spitting out JSON: " + await response.json());
+  console.log("Spitting out : " + await response.text());
+  
+  }
 
- }
-}
+
+};
+
